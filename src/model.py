fe6da7d507e7de8480444512956529e3cb580234
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-10-10 19:48:35 -0500

    initial commit

---
+++ src/model.py
@@ -0,0 +1,160 @@
+"""
+Image Similarity using Deep Ranking
+
+references: https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/42945.pdf
+
+@author: Zhenye Na
+"""
+
+import torch
+import torch.nn as nn
+
+
+def initialize_weights(module):
+    """Initialize weights."""
+    if isinstance(module, nn.Conv2d):
+        nn.init.xavier_normal(module.weight.data)
+    elif isinstance(module, nn.BatchNorm2d):
+        module.weight.data.fill_(1)
+        module.bias.data.zero_()
+    elif isinstance(module, nn.Linear):
+        module.bias.data.zero_()
+
+
+def conv3x3(in_channels, out_channels, stride=1):
+    """3x3 kernel size with padding convolutional layer in ResNet BasicBlock."""
+    return nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=3,
+        stride=stride,
+        padding=1,
+        bias=False)
+
+
+def resnet_cifar(**kwargs):
+    """Initialize ResNet model."""
+    model = ResNet(BasicBlock, [2, 4, 4, 2], **kwargs)
+    return model
+
+
+class BasicBlock(nn.Module):
+    """Basic Block of ReseNet."""
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        """Basic Block of ReseNet Builder."""
+        super(BasicBlock, self).__init__()
+
+        # First conv3x3 layer
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+
+        #  Batch Normalization
+        self.bn1 = nn.BatchNorm2d(num_features=out_channels)
+
+        # ReLU Activation Function
+        self.relu = nn.ReLU(inplace=True)
+
+        # Second conv3x3 layer
+        self.conv2 = conv3x3(out_channels, out_channels)
+
+        #  Batch Normalization
+        self.bn2 = nn.BatchNorm2d(num_features=out_channels)
+
+        # downsample for `residual`
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        """Forward Pass of Basic Block."""
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        return out
+
+
+class ResNet(nn.Module):
+    """Residual Neural Network."""
+
+    def __init__(self, block, duplicates, num_classes=100):
+        """Residual Neural Network Builder."""
+        super(ResNet, self).__init__()
+
+        self.in_channels = 32
+        self.conv1 = conv3x3(in_channels=3, out_channels=32)
+        self.bn = nn.BatchNorm2d(num_features=32)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout2d(p=0.02)
+
+        # block of Basic Blocks
+        self.conv2_x = self._make_block(block, duplicates[0], out_channels=32)
+        self.conv3_x = self._make_block(block, duplicates[1], out_channels=64, stride=2)
+        self.conv4_x = self._make_block(block, duplicates[2], out_channels=128, stride=2)
+        self.conv5_x = self._make_block(block, duplicates[3], out_channels=256, stride=2)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=4, stride=1)
+        self.fc_layer = nn.Linear(256, num_classes)
+
+        # initialize weights
+        # self.apply(initialize_weights)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal(m.weight.data, mode='fan_out')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+    def _make_block(self, block, duplicates, out_channels, stride=1):
+        """
+        Create Block in ResNet.
+        Args:
+            block: BasicBlock
+            duplicates: number of BasicBlock
+            out_channels: out channels of the block
+        Returns:
+            nn.Sequential(*layers)
+        """
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(num_features=out_channels)
+            )
+
+        layers = []
+        layers.append(
+            block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for _ in range(1, duplicates):
+            layers.append(block(out_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        """Forward pass of ResNet."""
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+
+        # Stacked Basic Blocks
+        out = self.conv2_x(out)
+        out = self.conv3_x(out)
+        out = self.conv4_x(out)
+        out = self.conv5_x(out)
+
+        out = self.maxpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc_layer(out)
+
+        return out
