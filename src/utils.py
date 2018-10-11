31b6997d0d11afac29ec8345e18cc248305cd2f8
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-10-11 09:19:38 -0500

    add plot support and custom Dataset in Pytorch

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,29 @@ references: https://static.googleusercontent.com/media/research.google.com/en//p
 
 @author: Zhenye Na
 """
+
+import os
+
+# from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataset import Dataset
+
+class TinyImageNet(Dataset):
+    """TinyImageNet."""
+
+    def __init__(self, root="/projects/training/bauh/tiny-imagenet-200/", transform=None):
+        """TinyImageNet Class Builder."""
+        # stuff
+        self.transform = transform
+        self.train_root = root + "train/"
+        self.test_root = root + "val/"
+
+    def __getitem__(self, index):
+        # stuff
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return (img, label)
+
+    def __len__(self):
+        return count # of how many examples(images?) you have
