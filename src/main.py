9d3d35438cc41ef57408d67c38d682b97cfc3892
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-10-16 13:48:37 -0500

    working in progress :construction:

--- src/main.py
+++ src/main.py
@@ -19,21 +19,27 @@ import argparse
 from utils import *
 from net import *
 
+
 parser = argparse.ArgumentParser()
 
 # directory
 parser.add_argument('--dataroot', type=str, default="../data", help='path to dataset')
 parser.add_argument('--ckptroot', type=str, default="../checkpoint/ckpt.t7", help='path to checkpoint')
+parser.add_argument('--trainroot', type=str, default="../tiny-imagenet-200", help='path to dataset')
 
 # hyperparameters settings
-parser.add_argument('--g', type=float, default=1.0, help='gap parameter')
 parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum factor')
 parser.add_argument('--nesterov', type=bool, default=True, help='enables Nesterov momentum')
 parser.add_argument('--weight_decay', type=float, default=1e-5, help='weight decay (L2 penalty)')
-parser.add_argument('--epochs', type=int, default=500, help='number of epochs to train')
+parser.add_argument('--epochs', type=int, default=35, help='number of epochs to train')
 parser.add_argument('--batch_size_train', type=int, default=129, help='training set input batch size')
-parser.add_argument('--batch_size_test', type=int, default=256, help='test set input batch size')
+parser.add_argument('--batch_size_test', type=int, default=129, help='test set input batch size')
+parser.add_argument('--start_epoch', type=int, default=0, help='starting epoch')
+
+# loss function settings
+parser.add_argument('--g', type=float, default=1.0, help='gap parameter')
+parser.add_argument('--p', type=int, default=2, help='norm degree for pairwise distance - Euclidean Distance')
 
 # training settings
 parser.add_argument('--resume', type=bool, default=False, help='whether re-training from ckpt')
@@ -49,8 +55,6 @@ args = parser.parse_args()
 def main():
     """Main pipeline of Image Similarity using Deep Ranking."""
 
-    start_epoch = 0
-
     # resume training from the last time
     if args.resume:
         # Load checkpoint
@@ -59,13 +63,13 @@ def main():
             '../checkpoint'), 'Error: no checkpoint directory found!'
         checkpoint = torch.load(args.ckptroot)
         net = checkpoint['net']
-        start_epoch = checkpoint['epoch']
+        args.start_epoch = checkpoint['epoch']
     else:
         # start over
-        print('==> Building new ResNet model ...')
-        net = Network()
+        print('==> Building new TripletNet model ...')
+        net = TripletNet(EmbeddingNet(resnet101()))
 
-    print("==> Initialize CUDA support for ResNet model ...")
+    print("==> Initialize CUDA support for TripletNet model ...")
 
     # For training on GPU, we need to transfer net and data onto the GPU
     # http://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#training-on-gpu
@@ -73,15 +77,22 @@ def main():
         net = torch.nn.DataParallel(net).cuda()
         cudnn.benchmark = True
 
-    trainloader, testloader = TinyImageNetLoader(train_root, test_root, batch_size_train, batch_size_test)
+    # Loss function, optimizer and scheduler
+    criterion = nn.TripletMarginLoss(margin=args.g, p=args.p)
+    optimizer = torch.optim.SGD(net.parameters(),
+                                lr=args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay,
+                                nesterov=args.nesterov)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                           mode='min',
+                                                           factor=0.1,
+                                                           patience=10,
+                                                           verbose=True)
 
+    trainloader, testloader = TinyImageNetLoader(args.trainroot, args.trainroot, args.batch_size_train, args.batch_size_test)
 
-    train(net, trainloader, testloader)
-
-
-
-
-
+    train(net, criterion, optimizer, scheduler, trainloader, testloader, args.start_epoch, args.epochs, args.is_gpu)
 
 
 
