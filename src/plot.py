810de7cfc1eb51ab9f2e2084d33dc90556ca3602
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-10-16 18:47:19 -0500

    update src

--- src/plot.py
+++ src/plot.py
@@ -9,20 +9,21 @@ references: https://static.googleusercontent.com/media/research.google.com/en//p
 
 import visdom
 import numpy as np
+
+
 vis = visdom.Visdom()
 vis.text('Hello, world!')
 vis.image(np.ones((3, 10, 10)))
 
 
 
-
-
 class VisdomLinePlotter(object):
     """Plots to Visdom"""
     def __init__(self, env_name='main'):
-        self.viz = Visdom()
+        self.viz = visdom.Visdom()
         self.env = env_name
         self.plots = {}
+
     def plot(self, var_name, split_name, x, y):
         if var_name not in self.plots:
             self.plots[var_name] = self.viz.line(X=np.array([x,x]), Y=np.array([y,y]), env=self.env, opts=dict(
@@ -33,49 +34,3 @@ class VisdomLinePlotter(object):
             ))
         else:
             self.viz.updateTrace(X=np.array([x]), Y=np.array([y]), env=self.env, win=self.plots[var_name], name=split_name)
-
-
-
-
-
-
-
-
-
-
-from pycrayon import CrayonClient
-import time
-
-# Connect to the server
-cc = CrayonClient(hostname="server_machine_address")
-
-# Create a new experiment
-foo = cc.create_experiment("foo")
-
-# Send some scalar values to the server
-foo.add_scalar_value("accuracy", 0, wall_time=11.3)
-foo.add_scalar_value("accuracy", 4, wall_time=12.3)
-# You can force the time and step values
-foo.add_scalar_value("accuracy", 6, wall_time=13.3, step=4)
-
-# Get the datas sent to the server
-foo.get_scalar_values("accuracy")
-# >>> [[11.3, 0, 0.0], [12.3, 1, 4.0], [13.3, 4, 6.0]])
-
-# backup this experiment as a zip file
-filename = foo.to_zip()
-
-# delete this experiment from the server
-cc.remove_experiment("foo")
-# using the `foo` object from now on will result in an error
-
-# Create a new experiment based on foo's backup
-bar = cc.create_experiment("bar", zip_file=filename)
-
-# Get the name of all scalar plots in this experiment
-bar.get_scalar_names()
-# >>> ["accuracy"]
-
-# Get the data for this experiment
-bar.get_scalar_values("accuracy")
-# >>> [[11.3, 0, 0.0], [12.3, 1, 4.0], [13.3, 4, 6.0]])
--- src/sampler.py
+++ src/sampler.py
@@ -6,35 +6,6 @@ references: https://static.googleusercontent.com/media/research.google.com/en//p
 @author: Zhenye Na
 """
 
-from torch.utils.data.sampler import Sampler
-
-class TripletSampler(Sampler):
-    """
-    Samples elements more than once in a single pass through the data.
-
-    This allows the number of samples per epoch to be larger than the number
-    of samples itself, which can be useful for data augmentation.
-    """
-    def __init__(self, nb_samples, desired_samples, shuffle=False):
-        self.data_samples = nb_samples
-        self.desired_samples = desired_samples
-        self.shuffle=shuffle
-
-    def _gen_sample_array(self):
-        n_repeats = self.desired_samples / self.data_samples
-        self.sample_idx_array = torch.range(0,self.data_samples-1).repeat(n_repeats).long()
-        # if self.shuffle:
-        #   self.sample_idx_array = self.sample_idx_array[torch.randperm(len(self.sample_idx_array)]
-        return self.sample_idx_array
-
-    def __iter__(self):
-        return iter(self._gen_sample_array())
-
-    def __len__(self):
-        return self.desired_samples
-
-
-
 import glob
 import json
 import random
@@ -44,94 +15,151 @@ import re
 import argparse
 import numpy as np
 
-def list_pictures(directory, ext='jpg|jpeg|bmp|png|ppm'):
+
+def list_pictures(directory, ext='JPEG'):
+    """
+    List of images in the directory.
+
+    Args:
+        directory: directory that contains images
+
+    Returns:
+        list of image names
+    """
     return [os.path.join(root, f)
             for root, _, files in os.walk(directory) for f in files
             if re.match(r'([\w]+\.(?:' + ext + '))', f)]
 
 
-def get_negative_images(all_images,image_names,num_neg_images):
+def get_negative_images(all_images, image_names, num_neg_images):
+    """
+    Sample negative images
+
+    Args:
+        all_images:
+        image_names:
+        num_neg_images:
+
+    Returns:
+        negative_images
+    """
     random_numbers = np.arange(len(all_images))
     np.random.shuffle(random_numbers)
-    if int(num_neg_images)>(len(all_images)-1):
-        num_neg_images = len(all_images)-1
+    if int(num_neg_images) > (len(all_images) - 1):
+        num_neg_images = len(all_images) - 1
+
     neg_count = 0
     negative_images = []
+
     for random_number in list(random_numbers):
         if all_images[random_number] not in image_names:
             negative_images.append(all_images[random_number])
             neg_count += 1
-            if neg_count>(int(num_neg_images)-1):
+            if neg_count > (int(num_neg_images) - 1):
                 break
+
     return negative_images
 
-def get_positive_images(image_name,image_names,num_pos_images):
+
+def get_positive_images(image_name, image_names, num_pos_images):
+    """
+    Sample positive images
+
+    Args:
+        all_images:
+        image_names:
+        num_pos_images:
+
+    Returns:
+        positive_images
+    """
+
     random_numbers = np.arange(len(image_names))
     np.random.shuffle(random_numbers)
-    if int(num_pos_images)>(len(image_names)-1):
-        num_pos_images = len(image_names)-1
+    if int(num_pos_images) > (len(image_names) - 1):
+        num_pos_images = len(image_names) - 1
+
     pos_count = 0
     positive_images = []
+
     for random_number in list(random_numbers):
         if image_names[random_number]!= image_name:
             positive_images.append(image_names[random_number])
             pos_count += 1
-            if int(pos_count)>(int(num_pos_images)-1):
+            if int(pos_count) > (int(num_pos_images) - 1):
                 break
+
     return positive_images
 
-def triplet_sampler(directory_path, output_path,num_neg_images,num_pos_images):
+
+def triplet_sampler(directory_path, output_path, num_neg_images, num_pos_images):
+    """
+    Perform Triplet Sampling.
+
+    Args:
+        directory_path: directory of Tiny ImageNet dataset
+        output_path: directory to save `triplets.txt`
+        num_neg_images: number of Negative images per Query image
+        num_pos_images: number of Positive images per Query image
+
+    Returns:
+        No return value, triplets will be written as txt file and be saved on disk.
+    """
     classes = [d for d in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, d))]
     all_images = []
     for class_ in classes:
-        all_images += (list_pictures(os.path.join(directory_path,class_)))
+        all_images += (list_pictures(os.path.join(directory_path, class_)))
+
     triplets = []
     for class_ in classes:
         image_names = list_pictures(os.path.join(directory_path,class_))
         for image_name in image_names:
             image_names_set = set(image_names)
             query_image = image_name
-            positive_images = get_positive_images(image_name,image_names,num_pos_images)
+            positive_images = get_positive_images(image_name, image_names, num_pos_images)
             for positive_image in positive_images:
-                negative_images = get_negative_images(all_images,set(image_names),num_neg_images)
+                negative_images = get_negative_images(all_images, set(image_names), num_neg_images)
                 for negative_image in negative_images:
-                    triplets.append(query_image+',')
-                    triplets.append(positive_image+',')
-                    triplets.append(negative_image+'\n')
+                    triplets.append(query_image + ',')
+                    triplets.append(positive_image + ',')
+                    triplets.append(negative_image + '\n')
 
+    print("==> Sampling Done ... Now Writing ...")
     f = open(os.path.join(output_path, "triplets.txt"), 'w')
     f.write("".join(triplets))
     f.close()
 
 
-if __name__ == '__main__':
+def main():
+    """Triplet Sampling."""
     # Instantiate the parser
-    parser = argparse.ArgumentParser(description='Optional app description')
+    parser = argparse.ArgumentParser(description='Triplet Sampler arguments')
 
-    parser.add_argument('--input_directory',
-                       help='A argument for input directory')
+    parser.add_argument('--input_directory', type=str, default="../tiny-imagenet-200/train",
+                       help='input directory')
 
-    parser.add_argument('--output_directory',
-                       help='A argument for output directory')
+    parser.add_argument('--output_directory', type=str, default="../",
+                       help='output directory')
 
-    parser.add_argument('--num_pos_images',
-                       help='A argument for the number of Positive images per Query image')
+    parser.add_argument('--num_pos_images', type=int, default=3,
+                       help='the number of Positive images per Query image')
 
-    parser.add_argument('--num_neg_images',
-                       help='A argument for the number of Negative images per Query image')
+    parser.add_argument('--num_neg_images', type=int, default=3,
+                       help='the number of Negative images per Query image')
 
     args = parser.parse_args()
 
-    if args.input_directory == None:
+
+    if args.input_directory is None:
         print('Input Directory path is required!')
         quit()
-    elif args.output_directory == None:
+    elif args.output_directory is None:
         print('Output Directory path is required!')
         quit()
-    elif args.num_pos_images == None:
+    elif args.num_pos_images is None:
         print('Number of Positive Images is required!')
         quit()
-    elif args.num_neg_images == None:
+    elif args.num_neg_images is None:
         print('Number of Negative Images is required!')
         quit()
     elif int(args.num_neg_images) < 1:
@@ -147,12 +175,16 @@ if __name__ == '__main__':
             print (args.input_directory+" path does not exist!")
             quit()
 
-    print ("Input Directory: "+args.input_directory)
-    print ("Output Directory: "+args.output_directory)
-    print ("Number of Positive image per Query image: "+args.num_pos_images)
-    print ("Number of Negative image per Query image: "+args.num_neg_images)
+    print ("Input Directory: " + args.input_directory)
+    print ("Output Directory: " + args.output_directory)
+    print ("Number of Positive image per Query image:", args.num_pos_images)
+    print ("Number of Negative image per Query image:", args.num_neg_images)
 
     triplet_sampler(directory_path=args.input_directory,
                     output_path=args.output_directory,
                     num_neg_images=args.num_neg_images,
                     num_pos_images=args.num_pos_images)
+
+
+if __name__ == '__main__':
+    main()
